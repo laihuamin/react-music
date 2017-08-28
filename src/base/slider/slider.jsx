86fd3737bd935192e7d8a12860822f916191fe19
laihuamin <1445509994@qq.com>
2017-08-28 17:53:31 +0800

    slider

--- src/base/slider/slider.jsx
+++ src/base/slider/slider.jsx
@@ -1,7 +1,25 @@
 import React, {Component} from 'react';
+import SliderItem from './sliderItem/sliderItem.jsx';
+import SliderDots from './sliderDots/SliderDots.jsx';
+import SliderArrow from './sliderArrow/sliderArrow.jsx';
 
 class Slider extends React.Component {
-    
+    constructor(props){
+        super(props);
+        this.state = {
+            nowLocal: 0
+        };
+    }
+    turn(n){
+        let _n = this.state.nowLocal + n;
+        if(_n < 0){
+            _n = _n + this.props.items.length;
+        }
+        if(_n > this.props.items.length){
+            _n = _n - this.props.items.length;
+        }
+        this.setState({nowLocal: _n});
+    }
 }
 
 
