fb9aac8e03c9fa098ca58998ee05bc843d644b04
laihuamin <1445509994@qq.com>
2017-09-10 23:52:01 +0800

    webpack proxy 服务端代理

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
+const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const extractText = new ExtractTextPlugin({
     filename: 'bundle.css'
 });
-const WebpackDevServer = require('webpack-dev-server');
+// const WebpackDevServer = require('webpack-dev-server');
 require('babel-polyfill');
+require('react-hot-loader');
 
 const htmlPlugin = new HTMLWebpackPlugin({
     template: './index.html',
@@ -14,7 +16,21 @@ const htmlPlugin = new HTMLWebpackPlugin({
 });
 
 module.exports = {
-    entry: ['babel-polyfill', './src/index.js'],
+    devtool: 'cheap-module-eval-source-map',
+    // devtool 指明了sourcemap的生成方式，它有七个选项，具体请参考 https://segmentfault.com/a/1190000004280859
+    // sourcemap 的作用就是为调试代码提供便利
+    // cheap-module-eval-source-map 绝大多数情况下都会是最好的选择，这也是下版本 webpack 的默认选项。
+
+
+    entry: [
+        'react-hot-loader',
+        //reload
+        'webpack-hot-middleware/client?reload=true',
+        // 这里reload=true的意思是，如果碰到不能hot reload的情况，就整页刷新。
+
+        'babel-polyfill', 
+        './src/index.js'
+    ],
 
     output: {
         filename: 'bundle.js',
@@ -45,23 +61,10 @@ module.exports = {
             }
         ]
     },
-    WebpackDevServer: {
-        historyApiFallback: true,
-        hot: true,
-        inline: true,
-        progress: true,
-        port: 3000,
-        host: '10.0.0.9',
-        proxy: {
-            '/recommend/*': {
-                target: 'http://localhost',
-                changeOrigin: true,
-                secure: false
-            }
-        }
-    },
 
     plugins: [
+        new webpack.HotModuleReplacementPlugin(),    //热启动，仅开发者模式
+        new webpack.NoEmitOnErrorsPlugin(),    // 允许错误不打断程序，,仅开发模式需要
         extractText,
         htmlPlugin
     ]
